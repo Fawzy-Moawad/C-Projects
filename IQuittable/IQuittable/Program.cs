cc8216113ab7bc139e15d545aa834e0f8391ef88
Fawzy Moawad <fawzy3arafa88@gmail.com>
2023-08-05 20:50:24 -0700

    add IQuittable project

---
+++ IQuittable/IQuittable/Program.cs
@@ -0,0 +1,38 @@
+﻿//Define the IQuittable interface
+public interface IQuittable
+{
+    void Quit();
+}
+
+//Define the Employee class and inherit the IQuittable interface
+public class Employee : IQuittable
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+    // Implement the Quit() method for the Employee class
+    public void Quit()
+    {
+        Console.WriteLine($"{FirstName} {LastName} is quitting the job.");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Create an object of type IQuittable and call the Quit() method
+        Employee employee = new Employee
+        {
+            FirstName = "John",
+            LastName = "Doe"
+        };
+
+        IQuittable quittable = employee; // Using polymorphism
+
+        quittable.Quit(); // This will call the Quit() method of the Employee class
+
+        // Keep the console window open
+        Console.ReadLine();
+    }
+}
